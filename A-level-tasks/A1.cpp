21e26b1c3c7cb96e93064c5493cafc22c35343e7
Kirill <sivkov.kirill@yandex.ru>
2016-10-06 23:18:10 +0300

    Fixing + improving A1
    Deleted unused code.

--- A-level-tasks/A1.cpp
+++ A-level-tasks/A1.cpp
@@ -1,11 +1,7 @@
-// A1.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
 #include <iostream>
 #include <cmath>
 
-unsigned long long int a, Result;
+long long a;
 unsigned int m, n;
 
 
@@ -13,9 +9,7 @@ int main()
 {
 	std::cin >> n >> m >> a;
 
-	Result = ceil(double(n) / a) * ceil(double(m) / a);
-
-	std::cout << Result << std::endl;
+	std::cout << ceil(double(n) / a) * ceil(double(m) / a) << std::endl;
 
     return 0;
 }
