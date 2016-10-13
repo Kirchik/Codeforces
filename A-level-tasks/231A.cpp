2d04d800f4d3835efc4662f6cbe94c2a8fa78df4
Kirill <sivkov.kirill@yandex.ru>
2016-10-13 21:09:24 +0300

    Create 231A.cpp

---
+++ A-level-tasks/231A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+int main()
+{
+	int n, a, b, c;
+	std::cin >> n;
+
+	int count = 0;
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> a >> b >> c;
+
+		if ((a + b + c) >= 2) 
+			++count; // 30ms
+		/* or (60ms)
+    if (a && b)
+			++count;
+		else if (b && c)
+			++count;
+		else if (c && a)
+			++count; */
+	}
+
+	std::cout << count;
+
+	return 0;
+}
